46161b24a2c4b0f1188198da5246796ff4c13650
juank1400 <juank1400@gmail.com>
2025-02-22 22:24:22 -0500

    Agregando constructor para serializar objeto order

--- lmax-lab/src/main/java/uandes/grupo4/Order.java
+++ lmax-lab/src/main/java/uandes/grupo4/Order.java
@@ -1,14 +1,21 @@
 package uandes.grupo4;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 // Clase que representa una orden
 public class Order {
     
-    public Order(String id, String type, String asset, int quantity) {
-        this.id = id;
-        this.type = type;
-        this.asset = asset;
-        this.quantity = quantity;
-    }
-
+    @JsonCreator
+    public Order(@JsonProperty("id") String id, 
+                 @JsonProperty("asset") String asset, 
+                 @JsonProperty("quantity") int quantity, 
+                 @JsonProperty("type") String type) {
+                    this.id = id;
+                    this.asset = asset;
+                    this.quantity = quantity;
+                    this.type = type;
+                }
     String id;
     String type; // "buy" o "sell"
     String asset;
