df86c7e0930a8a6ac95d3d01c568fcc02864a0d3
juank1400 <juank1400@gmail.com>
2025-02-23 17:40:39 -0500

    Documentacion

--- order-router/src/main/java/uandes/grupo4/Order.java
+++ order-router/src/main/java/uandes/grupo4/Order.java
@@ -1,5 +1,4 @@
 package uandes.grupo4;
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
