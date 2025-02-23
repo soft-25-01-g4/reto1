b26fb3308e912885a308d4344eb639225baa1e63
Brandon Rodriguez Aldana <rodriguezbrandon53@gmail.com>
2025-02-23 16:11:47 -0500

    Logica router

--- order-router/src/main/java/uandes/grupo4/OrderServiceClient.java
+++ order-router/src/main/java/uandes/grupo4/OrderServiceClient.java
@@ -1,16 +1,14 @@
 package uandes.grupo4;
-
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 
+@RegisterRestClient(configKey = "order-service")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public interface OrderServiceClient {
 
     @POST
-    @Path("/orders")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    void sendOrder(Order order);
+    Response forwardOrder(Order order);
 }
